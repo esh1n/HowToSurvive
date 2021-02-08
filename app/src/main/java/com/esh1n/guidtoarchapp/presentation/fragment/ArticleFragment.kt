b15028d841e1c3138eaa6d7905a96fadb6e17acc
esh1n <eshinsergey@gmail.com>
2021-02-08 17:46:26 +0300

    fixed display of article screen

--- app/src/main/java/com/esh1n/guidtoarchapp/presentation/fragment/ArticleFragment.kt
+++ app/src/main/java/com/esh1n/guidtoarchapp/presentation/fragment/ArticleFragment.kt
@@ -6,25 +6,23 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.esh1n.guidtoarchapp.R
 import com.esh1n.guidtoarchapp.presentation.adapter.ArticleContentAdapter
+import com.esh1n.guidtoarchapp.presentation.utils.UiUtils.adapter
+import com.esh1n.guidtoarchapp.presentation.utils.observeNonNull
 import com.esh1n.guidtoarchapp.presentation.viewmodel.ArticleViewModel
+import kotlinx.android.synthetic.main.fragment_article.*
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
 
     private val viewModel: ArticleViewModel by viewModels()
 
-    private lateinit var adapter: ArticleContentAdapter
-
     private val args: ArticleFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerview)
-        adapter = ArticleContentAdapter(requireActivity(), viewModel::toogleSavedState)
-        with(recyclerView) {
-            this.adapter = adapter
+        with(recyclerview) {
+            this.adapter = ArticleContentAdapter(requireActivity(), viewModel::toogleSavedState)
             layoutManager = LinearLayoutManager(requireActivity())
         }
     }
@@ -32,10 +30,8 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel.getArticleItems(args.articleId).observe(viewLifecycleOwner, {
-            it?.let {
-                adapter.setArticleItems(it)
-            }
+        viewModel.getArticleItems(args.articleId).observeNonNull(viewLifecycleOwner, {
+            recyclerview.adapter<ArticleContentAdapter>().setArticleItems(it)
         })
     }
 
